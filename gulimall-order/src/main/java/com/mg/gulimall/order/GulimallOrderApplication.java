136d801bda7b90179afc8079179a89c2d6cc4ab7
余松 <yusong@mg-pen.com>
2022-06-16 10:31:49 +0800

    初始化版本

---
+++ gulimall-order/src/main/java/com/mg/gulimall/order/GulimallOrderApplication.java
@@ -0,0 +1,13 @@
+package com.mg.gulimall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallOrderApplication.class, args);
+    }
+
+}
---
+++ gulimall-order/src/test/java/com/mg/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
