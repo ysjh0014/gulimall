64bf3c0136288584bead7e766bca0a9125b7c78e
ysjh0014 <3463446481@qq.com>
2022-09-23 16:48:46 +0800

    购物车服务启动类与配置项

---
+++ gulimall-cart/src/main/java/com/mg/gulimall/cart/GulimallCartApplication.java
@@ -0,0 +1,16 @@
+package com.mg.gulimall.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GulimallCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCartApplication.class, args);
+    }
+
+}
